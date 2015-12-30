23918394e33ea285c4a3441bcbdb90c75f05441b
bhaskarakepati <bhaskarakepati@gmail.com>
2015-12-30 23:05:21 +0000

    all files added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,19 +21,59 @@ var myapp = angular
     'ngMaterial','ngMessages'
   ]);
 
-myapp.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+function watchLanguageChange($scope,propertiesfactory){
+  $scope.$watch(function () { return propertiesfactory.getProperties("no"); },
+    function (value) {
+      $scope.properties = value;
+      console.log("$scope.properties in login con inside:"+$scope.properties);
+    }
+  );
+}
+
+myapp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/loginAgent', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginController'
+    }).
+    when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    }).
+    when('/restaurantDNIS', {
+      templateUrl: 'views/restaurants.html',
+      controller: 'RestaurantsController'
+    }).
+    when('/customerANI', {
+      templateUrl: 'views/customersani.html',
+      controller: 'RestaurantsController'
+    }).
+    when('/createcustomer', {
+      templateUrl: 'views/createcustomers.html',
+      controller: 'CustomersController'
+    }).
+    when('/updatecustomer', {
+      templateUrl: 'views/updatecustomer.html',
+      controller: 'CustomersController'
+    }).
+    when('/deletecustomer', {
+      templateUrl: 'views/deletecustomer.html',
+      controller: 'CustomersController'
+    }).
+    when('/createcustomeraddress', {
+      templateUrl: 'views/createcustomeraddress.html',
+      controller: 'CustomerAddressController'
+    }).
+    when('/updatecustomeraddress', {
+      templateUrl: 'views/updatecustomeraddress.html',
+      controller: 'CustomerAddressController'
+    }).
+    when('/deletecustomeraddress', {
+      templateUrl: 'views/deletecustomeraddress.html',
+      controller: 'CustomerAddressController'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
+  }]);
