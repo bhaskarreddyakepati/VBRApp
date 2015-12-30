5a9057882bf210457bc5108ab1f602b762bbb0d2
bhaskarakepati <bhaskarakepati@gmail.com>
2015-12-30 22:13:21 +0000

    INitial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testVbrappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testVbrappApp
+ */
+angular.module('testVbrappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testVbrappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testVbrappApp
+ */
+angular.module('testVbrappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
