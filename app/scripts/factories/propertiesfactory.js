69e4ca05ece96069b78a9a28db613723d7854559
bhaskarakepati <bhaskarakepati@gmail.com>
2015-12-31 01:34:47 +0000

    properties

--- app/scripts/factories/propertiesfactory.js
+++ app/scripts/factories/propertiesfactory.js
@@ -8,7 +8,7 @@ myapp.factory('propertiesfactory', function($http,$q) {
             if (language!="no") {
                 return $http({
                     method: "GET",
-                    url: "../resources/tillster-" + language + ".properties",
+                    url: "../assets/tillster-" + language + ".properties",
                 }).then(function (response) {
                     if (typeof response.data === 'object') {
                         properties = response.data;
